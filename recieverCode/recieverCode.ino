2c44c4d66749631cf3338e8765ad88f775fc7a5c
DK-3 <61070778+DK-3@users.noreply.github.com>
2020-12-31 14:46:13 -0600

    Save

--- recieverCode/recieverCode.ino
+++ recieverCode/recieverCode.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
 
+//const int servoPin = 7;
+
+RH_ASK driver;
+
+void setup()
+{
+    Serial.begin(9600);  // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+//    pinMode(servoPin, OUTPUT);     
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+    uint8_t buf[2];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+      //int i;
+      // Message with a good checksum received, dump it.
+      //Serial.print("Message: ");
+      Serial.println(strcmp((char*)buf, "BO"));
+      //Serial.println((char*)buf);
+      if(strcmp((char*)buf, "BP") == 0)
+        {
+          Serial.println("TX knows button is pressed");
+         //digitalWrite(servoPin, HIGH);
+        }
+     }
+    
 }
--- transmitterCode/transmitterCode.ino
+++ transmitterCode/transmitterCode.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
 
+const int buttonPin = 7;
+
+RH_ASK driver;
+
+void setup()
+{
+    Serial.begin(9600);    // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+    pinMode(buttonPin, INPUT);     
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    if(digitalRead(buttonPin)== HIGH)
+      {
+          const char *msg = "BP";
+          driver.send((uint8_t *)msg, strlen(msg));
+          driver.waitPacketSent();
+          delay(10);
+      }
+      if(digitalRead(buttonPin)== LOW)
+      {
+        const char *msg = "BO";
+        driver.send((uint8_t *)msg, strlen(msg));
+        driver.waitPacketSent();
+        delay(10);
+      }
 
 }
