7769bab7c147a2248ceb2940e85381ac2d180cd9
DK-3 <61070778+DK-3@users.noreply.github.com>
2021-01-02 13:51:08 -0600

    first servo integration

--- servoTesting/servoTesting.ino
+++ servoTesting/servoTesting.ino
@@ -10,10 +10,10 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos <= 25; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos <= 25; pos += 1) { // goes from 0 degrees to 25 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(5);                       // waits 15ms for the servo to reach the position
+    delay(5);                       // waits 5ms for the servo to reach the position
   }
   for (pos = 25; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
